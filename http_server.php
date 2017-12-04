4b53f2a262a68d82f655e27cdfaf42601c5e25d0
tom6262@163.com <tt16880880>
2017-12-04 11:16:00 +0800

    http server demo

---
+++ http_server.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pp
+ * Date: 2017/12/4
+ * Time: 11:12
+ */
+
+$http = new swoole_http_server('0.0.0.0', 9501);
+$http->on('request', function($request, $response){
+    var_dump($request->get, $request->post);
+    $response->header("Content-Type", "text/html; charset=utf-8");
+    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+});
+$http->start();
