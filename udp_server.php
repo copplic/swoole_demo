03152d334610b61b051b53b9b5eb67a7c075788a
tom6262@163.com <tt16880880>
2017-12-04 10:48:26 +0800

    udp server demo

--- udp_server.php
+++ udp_server.php
@@ -11,7 +11,7 @@ $serv = new swoole_server("127.0.0.1", 9502, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
 //监听数据接收事件
 $serv->on("Packet",function ($serv, $data, $clientInfo){
-    $serv->sentto($clientInfo['address'],$clientInfo['port'],"Server ".$data);
+    $serv->sendto($clientInfo['address'],$clientInfo['port'],"Server ".$data);
     var_dump($clientInfo);
 });
 
